9c6a334f5b5ab89f1d3bdeb8f079a888fb7a4ca4
AridSf <162722340+AridSf@users.noreply.github.com>
2024-03-25 19:06:54 +0300

    Update script.php
    update file script.php

--- script/script.php
+++ script/script.php
@@ -11,7 +11,7 @@ function redirect(string $path)
 function getPDO() : PDO
 {
     try {
-        return new PDO(dsn: "mysql:host=". DB_HOST .";dbname=". DB_NAME, username: DB_USERNAME, password: DB_PASSWORD);
+        return new PDO(dsn: "mysql:host=". DB_HOST .";dbname=". DB_NAME . ";username=" . DB_USERNAME . ";password=" . DB_PASSWORD);
     } catch (PDOException $e) {
         die($e -> getMessage());
     }
